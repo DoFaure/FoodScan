4e831288026ce6c6668d63e767dc6a8b8a72f8b1
DoFaure <dorianfaure69@gmail.com>
2021-01-06 13:29:36 +0100

    ajout du scan de code barre

---
+++ app/src/main/java/com/fr/foodscan/CaptureActivity.java
@@ -0,0 +1,4 @@
+package com.fr.foodscan;
+
+public class CaptureActivity extends com.journeyapps.barcodescanner.CaptureActivity {
+}
--- app/src/main/java/com/fr/foodscan/MainActivity.java
+++ app/src/main/java/com/fr/foodscan/MainActivity.java
@@ -1,15 +1,65 @@
 package com.fr.foodscan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.buttonScan)
+    Button mScanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScanButton = findViewById(R.id.buttonScan);
+        mScanButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        scanBarCode();
+    }
+
+    private void scanBarCode(){
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureActivity.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if(result.getContents() != null){
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(result.getContents());
+                builder.setTitle("Barcode scanned");
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }else{
+                Toast.makeText(this, "no result", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onActivityResult(requestCode,resultCode,data);
+        }
     }
 }
