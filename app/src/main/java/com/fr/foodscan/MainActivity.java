4f93647c3cde9778185a45dafe5921e6cae41400
DoFaure <dorianfaure69@gmail.com>
2021-01-06 11:48:47 +0100

    ajout logo

--- app/src/main/java/com/fr/foodscan/MainActivity.java
+++ app/src/main/java/com/fr/foodscan/MainActivity.java
@@ -1,8 +1,9 @@
 package com.fr.foodscan;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
